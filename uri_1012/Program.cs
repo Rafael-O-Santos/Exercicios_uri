7bcc3848683a3482aefdb697409d7a77b5c84298
RAFAEL OLIVEIRA DOS SANTOS <rafael.osantos25@senacsp.edu.br>
2022-09-01 17:20:51 -0300

    asd

--- uri_1012/Program.cs
+++ uri_1012/Program.cs
@@ -1,56 +1,34 @@
 ﻿using System;
-
+using System.Globalization;
 namespace uri_1012
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //---A B C---//
-            double A = double.Parse(Console.ReadLine());
-            double B = double.Parse(Console.ReadLine());
-            double C = double.Parse(Console.ReadLine());
+            string[] valores = Console.ReadLine().Split();
 
-            //---Área do Triângulo retângulo---//
+            double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            double pi = 3.14159;
 
-            double BASE = A;
-            double Altura = C;
-            double Area = BASE * Altura / 2;
+            //Formula triângulo retângulo
+            double atr = (a * c) / 2;
+            //Formula círculo retângulo
+            double cir = pi * (c * c);
+            //Formula trapézio
+            double trap = ((a + b) * c) / 2;
+            //Formula quadrado
+            double qua = b * b;
+            //Formula retângulo
+            double ret = a * b;
 
-            //---Área do círculo de raio C * pi---//
-
-            double Pi = 3.14159;
-            double Raio2 = C;
-            double Area2 = Pi * Raio2;
-
-            //---Área do trapézio---//
-
-            double bMA = A;
-            double bME = B;
-            double Area3 = (bMA + bME) / 2;
-
-            //---Área do quadrado---//
-
-            double Base = B;
-            double ALTURA = double.Parse(Console.ReadLine());
-            double Area4 = Base * ALTURA;
-
-            //---Área do retângulo---//
-
-            double Compri = A;
-            double Largura = B;
-            double Area5 = Compri * Largura;
-
-            //---Console---//
-
-            Console.WriteLine("TRIANGULO: " + Area);
-            Console.WriteLine("CIRCULO: " + Area2);
-            Console.WriteLine("TRAPEZIO: " + Area3);
-            Console.WriteLine("QUADRADO: " + Area4);
-            Console.WriteLine("RETANGULO: " + Area5);
-
-
-
+            Console.WriteLine("TRIAGULO: " + atr.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + atr.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + atr.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + atr.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + atr.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
