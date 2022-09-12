23dfe74bd6c81630ebd762a09df10d26ba5243d8
RAFAEL OLIVEIRA DOS SANTOS <rafael.osantos25@senacsp.edu.br>
2022-09-12 16:01:38 -0300

    asadaw

--- uri_1038/Program.cs
+++ uri_1038/Program.cs
@@ -6,7 +6,20 @@ namespace uri_1038
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double p1, p2, p3, p4, p5;
+
+            p1 = 4.00;
+            p2 = 4.50;
+            p3 = 5.00;
+            p4 = 2.00;
+            p5 = 1.50;
+
+            double qunt = double.Parse(Console.ReadLine());
+
+            double PrecoTotal = double.Parse(Console.ReadLine()) + qunt;
+
+            Console.WriteLine("Total: R$");
+          
         }
     }
 }
